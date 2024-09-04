f4f43a216aeef7390c0c19ca4aa871af0fdd38eb
Пирко Владимир <pirkovovan@gmail.com>
2024-09-04 03:21:48 +0300

    Refactor Lyndon factorization to return []lyndonSubstring (int pair) instead of []string Refactor corresponding test function to work with this new type

--- algorithms/lyndon_test.go
+++ algorithms/lyndon_test.go
@@ -6,15 +6,15 @@ import (
 )
 
 func TestLyndonFactorization(t *testing.T) {
-	factorizations := map[string]([]string){
-		"abcdef":         []string{"abcdef"},
-		"bababa":         []string{"b", "ab", "ab", "a"},
-		"aaa":            []string{"a", "a", "a"},
-		"abbaabbbaaabab": []string{"abb", "aabbb", "aaabab"},
+	factorizations := map[string]([]lyndonSubstring){
+		"abcdef":         []lyndonSubstring{{0, 5}},                         // abcdef
+		"bababa":         []lyndonSubstring{{0, 0}, {1, 2}, {3, 4}, {5, 5}}, // b ab ab a
+		"aaa":            []lyndonSubstring{{0, 0}, {1, 1}, {2, 2}},         // a a a
+		"abbaabbbaaabab": []lyndonSubstring{{0, 2}, {3, 7}, {8, 13}},        // abb aabbb aaabab
 	}
 
 	for str, factorization := range factorizations {
-		result := LyndonFactorization(str)
+		result := LyndonFactorization([]rune(str))
 		if !reflect.DeepEqual(result, factorization) {
 			t.Fatalf("wrong Lyndon factorization of \"%s\"\n\twant: %v\n\tgot: %v", str, factorization, result)
 		}
